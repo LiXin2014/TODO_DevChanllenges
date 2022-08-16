02feda26aa7683c608a13153e38c38272989775e
Xin Li <xinli601@gmail.com>
2022-08-15 22:07:00 -0700

    can add a new task

--- app/components/addTodo.js
+++ app/components/addTodo.js
@@ -1,20 +1,47 @@
 import React from "react";
+import Todo from "./todo";
+import { TodoState } from "./todoState";
 
 export default class AddTodo extends React.Component {
     constructor(props) {
         super(props);
 
-        this.setState({
-            text: ""
-        });
+        this.nextId = 1;
+        this.state = {text: ""};
+
+        this.onAddTodo = this.onAddTodo.bind(this);
+        this.onChange = this.onChange.bind(this);
     }
 
     render() {
         return (
             <div className="addTodo">
-                <input type="text" id="addTodo_Input" name="todo" placeholder="add details"/>
-                <button id="addTodo_Button" onClick={() =>{}}>Add</button>
+                <input type="text" 
+                    id="addTodo_Input" 
+                    name="todo" 
+                    placeholder="add details" 
+                    onChange={this.onChange} 
+                    value={this.state.text}/>
+                <button id="addTodo_Button" 
+                    onClick={() => this.onAddTodo()}>Add</button>
             </div>
         )
+    }
+
+    /* Handles the click event on Add button */
+    onAddTodo() {
+        let newTodo = new Todo(this.nextId++, this.state.text, TodoState.Active);
+        this.props.onAddTodo(newTodo);
+        this.setState({
+            text: ""
+        });
+    }
+
+    /* Handles the change event on input */
+    onChange(event) {
+        let text = event.target.value;
+        this.setState({
+            text
+        });
     }
 }
--- app/index.js
+++ app/index.js
@@ -4,17 +4,40 @@ import AddTodo from "./components/addTodo";
 import './index.css';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { todos: [] };
+
+        this.onAddTodo = this.onAddTodo.bind(this);
+    }
+
     render() {
         return (
             <div id="container">
                 <h2 id="header">#todo</h2>
-                <AddTodo />
+                <AddTodo onAddTodo={(newTodo) => this.onAddTodo(newTodo)} />
+                <ul>
+                    {this.state.todos.map((todo) => (
+                        <li>{todo.text}</li>
+                    ))}
+                </ul>
             </div>
         )
+    }
+
+    /* Add a todo item to the state */
+    onAddTodo(newTodo) {
+        this.setState((state) => {
+            let todos = state.todos;
+            todos.push(newTodo);
+            return {
+                todos
+            }
+        });
     }
 }
 
 ReactDom.render(
-    <App/>, 
+    <App />,
     document.getElementById('app')
 );
